f04b215526441f93ddc38b6c7784fb9970992b61
Arina <you@example.com>
2026-07-14 23:54:17 +0400

    Revert "move reverse string"
    This reverts commit 9e066d4411e1d621fdfb38ae744b71b2ed71d72f.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,13 +1,8 @@
 package com.example;
 
-import com.example.util.StringUtil;
-
 public class Main {
     static void main() {
         String s = "J@va the be$t!123";
-
-        String result = StringUtil.reversedLetters(s);
-        System.out.println(result);
 
 
 
