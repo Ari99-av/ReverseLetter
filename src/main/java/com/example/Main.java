1e973fd27743ad70a57e8602e4671a40b0b08cb9
Arina <you@example.com>
2026-07-28 00:37:03 +0400

    Add tests for reversed letters

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 import com.example.util.StringUtil;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         String s = "J@va the be$t!123";
 
         String result = StringUtil.reversedLetters(s);
